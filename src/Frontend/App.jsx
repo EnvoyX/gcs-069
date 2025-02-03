84095ed80a33bf2adca486dcb2d5a7a38cce88a1
Muhamad Hanif Hafizhan <muhamadhanifhafizhan@gmail.com>
2025-02-03 11:13:18 +0700

    added login page

--- src/Frontend/App.jsx
+++ src/Frontend/App.jsx
@@ -1,10 +1,15 @@
 import './App.css';
 import '../Backend/api/server';
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import {
+  Route,
+  createBrowserRouter,
+  createRoutesFromElements,
+  RouterProvider,
+} from 'react-router-dom';
 import Home from './pages/Home';
 import About from './pages/About';
 import Layout from './components/Layout';
-import Vans from './pages/Vans/Vans';
+import Vans, { loader as vansPageLoader } from './pages/Vans/Vans';
 import VansDetail from './pages/Vans/VansDetail';
 import Dashboard from './pages/Host/Dashboard';
 import Income from './pages/Host/Income';
@@ -16,36 +21,46 @@ import VansHostInfo from './pages/Host/VansHostInfo';
 import VansHostPhotos from './pages/Host/VansHostPhotos';
 import VansHostPricing from './pages/Host/VansHostPricing';
 import NotFound from './components/NotFound';
+import Error from './components/Error';
+import Login from './pages/Login';
+
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <>
+      {/* Nested Paths */}
+      <Route path="/" element={<Layout />}>
+        {/*Index routes render into their parent's <Outlet/> at their parent's URL*/}
+        <Route index element={<Home />}></Route>
+        {/* Relative Paths */}
+        <Route path="about" element={<About />}></Route>
+        <Route path="login" element={<Login />}></Route>
+        <Route
+          path="vans"
+          element={<Vans />}
+          loader={vansPageLoader}
+          errorElement={<Error></Error>}
+        ></Route>
+        {/* Route with params */}
+        <Route path="vans/:id" element={<VansDetail />}></Route>
+        <Route path="host" element={<HostLayout />}>
+          <Route index element={<Dashboard />} />
+          <Route path="income" element={<Income />} />
+          <Route path="reviews" element={<Reviews />} />
+          <Route path="vans" element={<VansHost />} />
+          <Route path="vans/:id" element={<VansHostDetail />}>
+            <Route index element={<VansHostInfo />}></Route>
+            <Route path="pricing" element={<VansHostPricing />}></Route>
+            <Route path="photos" element={<VansHostPhotos />}></Route>
+          </Route>
+        </Route>
+        <Route path="*" element={<NotFound></NotFound>}></Route>
+      </Route>
+    </>
+  )
+);
 
 function App() {
-  return (
-    <BrowserRouter>
-      <Routes>
-        {/* Nested Paths */}
-        <Route path="/" element={<Layout />}>
-          {/*Index routes render into their parent's <Outlet/> at their parent's URL*/}
-          <Route index element={<Home />}></Route>
-          {/* Relative Paths */}
-          <Route path="about" element={<About />}></Route>
-          <Route path="vans" element={<Vans />}></Route>
-          {/* Route with params */}
-          <Route path="vans/:id" element={<VansDetail />}></Route>
-          <Route path="host" element={<HostLayout />}>
-            <Route index element={<Dashboard />} />
-            <Route path="income" element={<Income />} />
-            <Route path="reviews" element={<Reviews />} />
-            <Route path="vans" element={<VansHost />} />
-            <Route path="vans/:id" element={<VansHostDetail />}>
-              <Route index element={<VansHostInfo />}></Route>
-              <Route path="pricing" element={<VansHostPricing />}></Route>
-              <Route path="photos" element={<VansHostPhotos />}></Route>
-            </Route>
-          </Route>
-          <Route path="*" element={<NotFound></NotFound>}></Route>
-        </Route>
-      </Routes>
-    </BrowserRouter>
-  );
+  return <RouterProvider router={router}></RouterProvider>;
 }
 
 export default App;
