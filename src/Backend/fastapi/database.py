04480429cc083aa8703a43a399ac2917ff86ab1b
Muhamad Hanif Hafizhan <muhamadhanifhafizhan@gmail.com>
2025-02-04 20:33:29 +0700

    update requirements for backend

--- src/Backend/fastapi/database.py
+++ src/Backend/fastapi/database.py
@@ -2,7 +2,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-URL_DATABASE = "postgresql://postgres:BackendPusing2--5@localhost:5432/postgres"
+URL_DATABASE = "postgresql://postgres:SemogaBisaFullstack2--5@localhost:5432/postgres"
 engine = create_engine(URL_DATABASE)
 
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
