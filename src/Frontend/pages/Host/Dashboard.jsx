d423a4924da12770aa350e7ada733847c2ad747a
Muhamad Hanif Hafizhan <muhamadhanifhafizhan@gmail.com>
2025-02-06 17:14:12 +0700

    delete react-icons to avoid issus

--- src/Frontend/pages/Host/Dashboard.jsx
+++ src/Frontend/pages/Host/Dashboard.jsx
@@ -1,6 +1,5 @@
-import { Link } from 'react-router-dom';
-import { BsStarFill } from 'react-icons/bs';
-import { getDataVans } from '../../lib/getVan';
+import { Link } from "react-router-dom";
+import { getDataVans } from "../../lib/getVan";
 
 export default function Dashboard() {
   const vans = getDataVans();
@@ -30,7 +29,18 @@ export default function Dashboard() {
       </section>
       <section className="host-dashboard-reviews">
         <h2>Review score</h2>
-        <BsStarFill className="star" />
+        <svg
+          stroke="#ff8c38"
+          fill="#ff8c38"
+          strokeWidth="0"
+          viewBox="0 0 16 16"
+          height="25px"
+          width="25px"
+          xmlns="http://www.w3.org/2000/svg"
+          className="star"
+        >
+          <path d="M3.612 15.443c-.386.198-.824-.149-.746-.592l.83-4.73L.173 6.765c-.329-.314-.158-.888.283-.95l4.898-.696L7.538.792c.197-.39.73-.39.927 0l2.184 4.327 4.898.696c.441.062.612.636.282.95l-3.522 3.356.83 4.73c.078.443-.36.79-.746.592L8 13.187l-4.389 2.256z"></path>
+        </svg>
         <p>
           <span>5.0</span>/5
         </p>
--- src/Frontend/pages/Host/Reviews.jsx
+++ src/Frontend/pages/Host/Reviews.jsx
@@ -1,21 +1,20 @@
-import { BsStarFill } from 'react-icons/bs';
-import reviewImg from '/reviews-graph.png';
+import reviewImg from "/reviews-graph.png";
 
 export default function Reviews() {
   const reviewsData = [
     {
       rating: 5,
-      name: 'Elliot',
-      date: 'January 3, 2023',
-      text: 'The beach bum is such an awesome van! Such a comfortable trip. We had it for 2 weeks and there was not a single issue. Super clean when we picked it up and the host is very comfortable and understanding. Highly recommend!',
-      id: '1',
+      name: "Elliot",
+      date: "January 3, 2023",
+      text: "The beach bum is such an awesome van! Such a comfortable trip. We had it for 2 weeks and there was not a single issue. Super clean when we picked it up and the host is very comfortable and understanding. Highly recommend!",
+      id: "1",
     },
     {
       rating: 5,
-      name: 'Sandy',
-      date: 'December 12, 2022',
-      text: 'This is our third time using the Modest Explorer for our travels and we love it! No complaints, absolutely perfect!',
-      id: '2',
+      name: "Sandy",
+      date: "December 12, 2022",
+      text: "This is our third time using the Modest Explorer for our travels and we love it! No complaints, absolutely perfect!",
+      id: "2",
     },
   ];
 
@@ -34,7 +33,19 @@ export default function Reviews() {
           <div className="review">
             <div className="flex mb-3">
               {[...Array(review.rating)].map((_, i) => (
-                <BsStarFill className="review-star" key={i} />
+                <svg
+                  key={i}
+                  stroke="#ff8c38"
+                  fill="#ff8c38"
+                  strokeWidth="0"
+                  viewBox="0 0 16 16"
+                  height="25px"
+                  width="25px"
+                  xmlns="http://www.w3.org/2000/svg"
+                  className="star"
+                >
+                  <path d="M3.612 15.443c-.386.198-.824-.149-.746-.592l.83-4.73L.173 6.765c-.329-.314-.158-.888.283-.95l4.898-.696L7.538.792c.197-.39.73-.39.927 0l2.184 4.327 4.898.696c.441.062.612.636.282.95l-3.522 3.356.83 4.73c.078.443-.36.79-.746.592L8 13.187l-4.389 2.256z"></path>
+                </svg>
               ))}
             </div>
             <div className="info">
